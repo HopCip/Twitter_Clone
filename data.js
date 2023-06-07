6cf591cb16f636c4cfa7ef01418b2a6e5e86ced9
Ľubomír Hlavina <116902235+HopCip@users.noreply.github.com>
2023-06-07 23:21:48 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,57 @@
+export const tweetsData = [   
+    {
+        handle: `@Josh526 💎`,
+        profilePic: `images/shrek.jpg`,
+        likes: 27,
+        retweets: 10,
+        tweetText: `Before I got married I didn't even know there was a wrong way to put the milk back in the fridge 😪`,
+        replies: [],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '4b161eee-c0f5-4545-9c4b-8562944223ee',
+    },    
+    {
+        handle: `@Elon ✅`,
+        profilePic: `images/musk.png`,
+        likes: 6500,
+        retweets: 234,
+        tweetText: `Dubai is where sad rich people go. Happy rich people go to places like Hawaii and surf 🌊`,
+        replies: [
+                  {
+                handle: `@Micro_Billy`,
+                profilePic: `images/billy.jpg`,
+                tweetText: `Where do sad poor people go?`,
+            },
+                  {
+                handle: `@Harry_Smith546`,
+                profilePic: `images/spongebob-squarepant.jpg`,
+                tweetText: `To work...`,
+            },
+        ],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '3c23454ee-c0f5-9g9g-9c4b-77835tgs2',
+    },
+        {
+        handle: `@Charlie_5555`,
+        profilePic: `images/flower.png`,
+        likes: 10,
+        retweets: 3,
+        tweetText: `My roommate just finished writing a book on "How to earn money" and now he needs money to publish it! 📕`,
+        replies: [
+            {
+                handle: `@Ronald_69`,
+                profilePic: `images/parks.png`,
+                tweetText: `Tell him to read the book`,
+            },
+            {
+                handle: `@Lewis_Murray3`,
+                profilePic: `images/love.png`,
+                tweetText: `😂😂😂😂😂😂😂`,
+            },
+        ],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '8hy671sff-c0f5-4545-9c4b-1237gyys45',
+    },     
+]
